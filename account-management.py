af6ca6fc3a8dee20e501b270dc83b7308c919327
Giovanni <giovanni.andrade9991@gmail.com>
2022-04-12 21:11:46 -0300

    Initial commit

--- account-management.py
+++ account-management.py
@@ -1,4 +1,13 @@
 #db connect
+from time import sleep
+from selenium import webdriver
 
+browser = webdriver.Firefox()
+
+browser.get('https://www.instagram.com/')
+
+sleep(5)
+
+browser.close()
 
 #crud
